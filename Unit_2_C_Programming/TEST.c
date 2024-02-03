a19206dad51d19fa21cc7cd05654be67bacb0235
TawfikTamer <tawfik.mahros04@eng-st.cu.edu.eg>
2024-02-03 02:06:06 +0200

    test

---
+++ Unit_2_C_Programming/TEST.c
@@ -0,0 +1,24 @@
+/*
+ * TEST.c
+ *
+ *  Created on: ٣١‏/٠١‏/٢٠٢٤
+ *      Author: TAWFIK
+ */
+
+
+#include "stdio.h"
+
+void main()
+{
+	int i,j;
+	for(i=0 ; i<10;i++)
+	{
+		for(j=i ; j<10 ; j++)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+
+	}
+
+}
