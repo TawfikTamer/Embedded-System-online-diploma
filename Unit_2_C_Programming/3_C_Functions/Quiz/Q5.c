5f821fd082bdcd3d4dc030c6948cba0bb890176a
TawfikTamer <tawfik.mahros04@eng-st.cu.edu.eg>
2024-02-08 16:42:10 +0200

    update

--- Unit_2_C_Programming/3_C_Functions/Quiz/Q5.c
+++ Unit_2_C_Programming/3_C_Functions/Quiz/Q5.c
@@ -9,7 +9,7 @@
 #include "stdio.h"
 
 int clr_bit(int , int);
-
+void show_bin(int);
 int main()
 {
 	int num,bit;
@@ -17,11 +17,17 @@ int main()
 	printf("number: ");
 	fflush(stdout);
 	scanf("%d",&num);
-	printf("Bit position: ");
+	printf("Binary number for %d is:\n",num);
+	show_bin(num);
+
+	printf("\nBit position: ");
 	fflush(stdout);
 	scanf("%d",&bit);
 
-	printf("number after clearing the bit is %d",clr_bit(num,bit));
+	printf("number after clearing the bit is %d\n",clr_bit(num,bit));
+	printf("Binary number after clear is:\n");
+	show_bin(clr_bit(num,bit));
+
 
 	return 0;
 }
@@ -36,4 +42,17 @@ int clr_bit(int x , int y)
 		x &= ~(1<<y);
 		return x;
 	}
+}
+
+void show_bin(int x)
+{
+	int i;
+	for(i=sizeof(x)*8;i>0;i--)
+	{
+		if(x &(1<<(i-1)))
+			printf("1");
+		else
+			printf("0");
+	}
+
 }
--- Unit_2_C_Programming/3_C_Functions/Quiz/Q6.c
+++ Unit_2_C_Programming/3_C_Functions/Quiz/Q6.c
@@ -9,6 +9,7 @@
 #include "stdio.h"
 
 int Bit_stat(int);
+void show_bin(int);
 
 int main()
 {
@@ -16,8 +17,10 @@ int main()
 	printf("number: ");
 	fflush(stdout);
 	scanf("%d",&num);
+	printf("Binary number for %d is:\n",num);
+	show_bin(num);
 
-	printf("The 4th bit in number %d is %d",num , Bit_stat(num));
+	printf("\nThe 4th bit in number %d is %d",num , Bit_stat(num));
 
 	return 0;
 }
@@ -29,4 +32,17 @@ int Bit_stat(int x)
 	stat =1;
 
 	return stat;
+}
+
+void show_bin(int x)
+{
+	int i;
+	for(i=sizeof(x)*8;i>0;i--)
+	{
+		if(x &(1<<(i-1)))
+			printf("1");
+		else
+			printf("0");
+	}
+
 }
