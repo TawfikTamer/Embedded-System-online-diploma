8ef6cbd7c8ac01ccd6ba7644d3ab780a6d2d75fc
TawfikTamer <tawfik.mahros04@eng-st.cu.edu.eg>
2024-07-27 23:50:09 +0300

    Sec 2 Drivers

---
+++ Unit_7_MCU_Essential_Peripherals/Lesson_2_GPIO_2/Key_PAD_Driver/KPD.h
@@ -0,0 +1,18 @@
+/*
+ *<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<    KPD.c    >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+ *
+ *  Author : Tawfik tamer ibrahim
+ *  Layer  : HAL
+ *  SWC    : KPD
+ *
+ */
+
+#ifndef KPD_H_
+#define KPD_H_
+
+#define NOTPRESSED 0xff
+
+void KPD_Init(void);
+u8   KPD_u8GetPressed( void );
+
+#endif /* KPD_H_ */
