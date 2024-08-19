88e9120aadd3720eac0641c68c8cf2a3ede97a7c
TawfikTamer <tawfik.mahros04@eng-st.cu.edu.eg>
2024-08-20 02:29:17 +0300

    ATmega32 UART driver and section

---
+++ Unit_8_MCU_Interfacing/UART/SEC/Software/Debug/MCAL/USART/USART_program.d
@@ -0,0 +1,32 @@
+MCAL/USART/USART_program.d MCAL/USART/USART_program.o: \
+ ../MCAL/USART/USART_program.c ../MCAL/USART/../../LIB/std_types.h \
+ ../MCAL/USART/../../LIB/common_macros.h ../MCAL/USART/USART_config.h \
+ ../MCAL/USART/USART_interface.h ../MCAL/USART/USART_private.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+../MCAL/USART/../../LIB/std_types.h:
+
+../MCAL/USART/../../LIB/common_macros.h:
+
+../MCAL/USART/USART_config.h:
+
+../MCAL/USART/USART_interface.h:
+
+../MCAL/USART/USART_private.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
---
+++ Unit_8_MCU_Interfacing/UART/SEC/Software/Debug/makedep.mk
@@ -0,0 +1,12 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+HAL\LCD\CLCD_program.c
+
+main.c
+
+MCAL\DIO\DIO_program.c
+
+MCAL\USART\USART_program.c
+
